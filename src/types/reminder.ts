53845fc5d89351d6bcf78df50f5132f833a3dac9
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-05-01 19:40:36 -0300

    Adds city and date to reminder companion

--- src/types/reminder.ts
+++ src/types/reminder.ts
@@ -2,16 +2,23 @@ export interface Reminder {
   title: string;
   id: string;
   date: Date;
+  color: string;
+  city: string;
 }
 
 export interface ReminderCompanion {
   title?: string;
+  color?: string;
+  city?: string;
+  date?: Date;
 }
 
 export const replaceValues = (reminder: Reminder, companion: ReminderCompanion): Reminder => {
   return {
     title: companion.title ?? reminder.title,
-    date: reminder.date,
+    color: companion.color ?? reminder.color,
+    date: companion.date ?? reminder.date,
+    city: companion.city ?? reminder.city,
     id: reminder.id,
   };
 };
